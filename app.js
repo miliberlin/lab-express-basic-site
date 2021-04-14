5effb4845404097a747b5a361019ed63b383f7c3
miliberlin <michelle.liebheit@posteo.de>
2021-04-14 16:46:15 +0200

    done with lab requirements

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (request,response) => {
     response.sendFile(__dirname + '/views/home-page.html')
 })
+app.get('/about', (request,response) => {
+    response.sendFile(__dirname + '/views/about-page.html')
+})
+app.get('/gallery', (request,response) => {
+    response.sendFile(__dirname + '/views/gallery-page.html')
+})
+app.get('/works', (request,response) => {
+    response.sendFile(__dirname + '/views/works-page.html')
+})
 
 app.listen(3000, () => 
     console.log('Server listening')
